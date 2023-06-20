0e092d87cfdbda750a77f8dd201a99a95af3a5a9
Robinson Regalado <rregalado17@gmail.com>
2023-06-19 20:19:27 -0600

    draft skeletal outline of site

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import About from "./About";
+import Bio from "./Bio";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Projects from "./Projects";
+import Techstack from "./Techstack";
+
+export {
+    About,
+    Bio,
+    Contact,
+    Footer,
+    Navbar,
+    Projects,
+    Techstack
+};
