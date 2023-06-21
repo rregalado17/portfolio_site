a80e8f22008862910cb21490da7b63dbd3dc8cdf
Robinson Regalado <rregalado17@gmail.com>
2023-06-20 20:49:24 -0600

    finalize navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,9 @@ import { navLinks } from '../constants';
 import { close, logo, menu } from '../assets'; 
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt='rregalado' className='w-[124px] h-[32px]'/>
@@ -21,8 +24,31 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-      <div>
-        
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <img src={ toggle ? close : menu} 
+          alt="menu"
+          className='w-[28px] h-[28px] object-contain'
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div 
+          className={`${toggle ? 'flex' : 'hidden'} 
+            p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+        <ul className='list-none flex flex-col justify-end items-center flex-1'>
+          {navLinks.map((nav, index) => (
+            <li
+              key={ nav.id }
+              className={ `font-poppins
+              font-normal
+              cursor-pointer
+              text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'}
+              text-white`}
+            >
+              <a href={`#${nav.id}`}> {nav.title} </a>
+            </li>
+          ))}
+        </ul>
+        </div>
       </div>
     </nav>
   )
