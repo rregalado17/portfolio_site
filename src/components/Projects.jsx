c5e2920f729acd63e11259167d563b765e54c7e2
Robinson Regalado <rregalado17@gmail.com>
2023-06-26 22:41:16 -0600

    update card info

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { feedback } from '../constants'
+import styles from '../style'
+import CardInfo from './CardInfo'
 
-const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
 
+const Projects = () => (
+  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+    <div></div>
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h1 className={styles.heading2}>Here are a list of my projects <br className='sm:block hidden'/>using various different technologies!</h1>
+    </div>
+  </section>
+)
 export default Projects
--- src/constants/index.js
+++ src/constants/index.js
@@ -49,26 +49,26 @@ export const features = [
 
 export const feedback = [
   {
-    id: "feedback-1",
+    id: "project-1",
     content:
-      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Herman Jensen",
+      "MVC Rails web app that lets users create an account and browse vacation packages within your budget.",
+    name: "Local Scout",
     title: "Founder & Leader",
     img: people01,
   },
   {
-    id: "feedback-2",
+    id: "project-2",
     content:
-      "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
+      "MVC Ruby on Rails web app that lets user create an account, comment on cafes and interact with others.",
+    name: "Remote Cafe",
     title: "Founder & Leader",
     img: people02,
   },
   {
-    id: "feedback-3",
+    id: "project-3",
     content:
-      "It is usually people in the money business, finance, and international trade that are really rich.",
-    name: "Kenn Gallagher",
+      "Rails backend & React frontend single-page web app allowing users build lawyers and litigation profiles.",
+    name: "Litigation Manager",
     title: "Founder & Leader",
     img: people03,
   },
