59a3281b5ae2ea975ee506efffb21f6825f80735
Robinson Regalado <rregalado17@gmail.com>
2023-06-27 22:35:27 -0600

    draft cards for medium blog posts

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 // import { Card, CardImg, CardBody, CardTitle, CardSubtitle, Row, Col } from "reactstrap";
 // var moment = require("moment");
+import styles from "../style";
 
 const Blog = () => {
   const [mediumData, setMediumData] = useState([]);
@@ -22,26 +23,34 @@ const Blog = () => {
   return (
     <div id="blog" className="container mt-3">
       <h3 className="ui horizontal header divider mt-5">
-        <p>Blog</p>
+        Blog
       </h3>
+      
       {isLoading && <p>Fetching data from Medium!</p>}
-
+      <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} flex flex-wrap sm:justify-start justify-center w-full relative z-[1] feedback-container bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <h2 className={`${styles.heading2}`}>Techstack</h2>
+        
         {finalData.map(article => (
-          <div md="4" className="mb-3" key={article.guid}>
-            <div>
+        <div md="4" className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
+            
+
+<div className='flex-1 flex flex-col'>
+
                 <img top width="100%" src={article.thumbnail} alt="img" />
                   <div>
                     <a href={article.link}>{article.title}</a>
                     </div>
-       
-                  <p>
+                  <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
                     Published:{" "}
-                    (article.pubDate).format("dddd, MMMM Do YYYY")d
+                    {/* {moment(article.pubDate).format("dddd, MMMM Do YYYY")} */}
+                    (article.pubDate).format("dddd, MMMM Do YYYY")
+
                   </p>
 
             </div>
           </div>
         ))}
+        </section>
     </div>
   );
 };
---
+++ src/components/Tech.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+// import { Card, CardImg, CardBody, CardTitle, CardSubtitle, Row, Col } from "reactstrap";
+// var moment = require("moment");
+import styles from "../style";
+
+const Tech = () => {
+  return (
+    <div>Tech</div>
+  )
+}
+
+export default Tech
